f9414aef8d028ead657d4cc86290b7190f3cda3c
koyuncuoglum95 <koyuncuoglum@outlook.com>
2023-07-16 16:06:45 -0700

    Part 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,181 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <div>
+      <h1 className="border-4 border-yellow bg-blue text-purple text-8xl rounded-4xl border-spacing-128 md:text-9xl md:rounded-none">
+        Hello world!
+      </h1>
+
+      <article className="prose lg:prose-xl">
+        <h1 className='text-white'>Garlic bread with cheese: What the science tells us</h1>
+        <p className='text-white'>For years parents have espoused the health benefits of eating garlic bread with cheese to their
+          children, with the food earning such an iconic status in our culture that kids will often dress
+          up as warm, cheesy loaf for Halloween.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+        <p className='text-white'>But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
+          springing up around the country.
+        </p>
+      </article>
+
+
+      <div className="aspect-w-16 aspect-h-9">
+        <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+
+      <div>
+        <h1 className='text-3xl'>Title 1</h1>
+        <h2 className='text-2xl'>Title 2</h2>
+        <h3 className='text-xl'>Title 3</h3>
+        <p className='text-base capitalize'>a regular paragraph</p>
+        <p className='text-sm'>A Description paragraph</p>
+        <p className='text-xs'>A little note</p>
+      </div>
+
+      <div>
+        <p className="italic underline decoration-green decoration-dotted underline-offset-1 leading-6">The quick brown fox ...</p>
+        <p className="not-italic hover:not-italic underline  hover:no-underline decoration-wavy underline-offset-4 leading-8">The quick brown fox ...</p>
+      </div>
+
+      <div className="pt-6">pt-6</div>
+
+      <div className="pr-4">pr-4</div>
+
+      <div className="pb-8">pb-8</div>
+
+      <div className="pl-2 ...">pl-2</div>
+
+      <div className="px-8 ...">px-8</div>
+
+      <div className="py-8 ...">py-8</div>
+
+      <div className="p-8 ...">p-8</div>
+
+
+      <div className="mt-6 ...">mt-6</div>
+      <div className="mr-4 ...">mr-4</div>
+      <div className="mb-8 ...">mb-8</div>
+      <div className="ml-2 ...">ml-2</div>
+
+      <div className="mx-8 ...">mx-8</div>
+
+      <div className="my-8 ...">my-8</div>
+
+      <div className="m-8 ...">m-8</div>
+
+      <div className="w-36 h-16 bg-sky-400 opacity-20 ..."></div>
+      <div className="-mt-8 bg-sky-300 ...">-mt-8</div>
+
+
+      <div className="flex space-x-4 ...">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+      <div className="flex flex-col space-y-4 ...">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+      <div className="flex flex-row-reverse space-x-4 space-x-reverse ...">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+      <div className="flex flex-row ...">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+
+      <div className="flex flex-row-reverse ...">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+      <div className="flex flex-col ...">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+
+      <div className="flex flex-col-reverse ...">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+
+      <div className="flex flex-row">
+        <div className="basis-1/4">01</div>
+        <div className="basis-1/4">02</div>
+        <div className="basis-1/2">03</div>
+      </div>
+      
+      <div className="flex flex-nowrap">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+      <div className="flex flex-wrap">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+      <div className="flex flex-wrap-reverse">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+
+
+      <div className="flex">
+        <div className="flex-none w-14 h-14">
+          01
+        </div>
+        
+        <div className="flex-initial w-64 ...">
+          02
+        </div>
+        <div className="flex-initial w-32 ...">
+          03
+        </div>
+      </div>
+   
+      <div className="flex">
+        <div className="flex-none ...">
+          01
+        </div>
+        <div className="flex-1 w-64 ...">
+          02
+        </div>
+        <div className="flex-1 w-32 ...">
+          03
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+      <div className="flex ...">
+        <div className="flex-none w-14 h-14 ...">
+          01
+        </div>
+        <div className="flex-none ...">
+          02
+        </div>
+        <div className="flex-1 ...">
+          03
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+    
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,54 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white': '#ffffff',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {
+      fontSize: {
+        xs: '12px',
+        sm: '15px',
+        base: '18px',
+        lg: '21px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+       spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
 }
